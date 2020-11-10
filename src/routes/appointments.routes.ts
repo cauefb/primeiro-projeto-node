d47d34ab1fac0c820e1b2abdd7bf94f55a4fb2d8
cauefb <kau3hc@hotmail.com>
2020-11-10 16:56:28 -0300

    validando data

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  console.log(parsedDate);
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+  console.log(findAppointmentInSameDate);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+  return response.json(appointment);
+});
+export default appointmentsRouter;
